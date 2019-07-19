c90fefcea8d6031f57b5e2e23d26f8eb1a14c61e
Oleksiy Drozdov <oleksiydrozdov@gamil.com>
2019-07-19 20:29:58 +0200

    Updates

---
+++ precache-manifest.c144d20c59c7c9b2525337cbc02ba272.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796e6f2c054a29c66cbb7281c960a0a1",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "955d0f993eae6c837ae2",
+    "url": "/todo/static/css/main.ec9d8267.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/todo/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "955d0f993eae6c837ae2",
+    "url": "/todo/static/js/main.54b48852.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
